60afbe536ec68f34c13fd5ea06ca781aacc75442
AndreiDetenkov <32195682+AndreiDetenkov@users.noreply.github.com>
2024-01-29 23:45:38 +0600

    feat: implement skin-care landing

--- src/components/header/HeaderLogo.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-import { ref } from 'vue'
-
-const logo = ref('Beuitty.')
-</script>
-
-<template>
-  <div class="text-2xl font-black text-white">{{ logo }}</div>
-</template>
