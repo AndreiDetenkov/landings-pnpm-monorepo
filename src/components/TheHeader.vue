4af2a80ccad6e153fe50f81708d3e888871789b8
AndreiDetenkov <32195682+AndreiDetenkov@users.noreply.github.com>
2024-01-29 22:09:35 +0600

    refactor: small updates in header component

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,18 +1,23 @@
 <script setup lang="ts">
+const navigation: Array<{name: string, href: string}> = [
+  { name: 'Home', href: '#home' },
+  { name: 'Product', href: '#product' },
+  { name: 'About', href: '#about' },
+  { name: 'Contact', href: '#contact' },
+]
 </script>
 
 <template>
   <header class="bg-gradient-to-r from-[#101820] to-[#553f38] py-8">
       <div class="container">
-        <div class="flex justify-between">
+        <div class="flex justify-between items-center">
           <div class="text-2xl font-black text-white">Beuitty.</div>
 
           <nav>
             <ul class="inline-flex gap-x-14 text-[#898A92] pr-14">
-              <li class="hover:text-white transition text-white"><a href="#">Home</a></li>
-              <li class="hover:text-white transition"><a href="#">Product</a></li>
-              <li class="hover:text-white transition"><a href="#">About</a></li>
-              <li class="hover:text-white transition"><a href="#">Contact</a></li>
+              <li v-for="{name, href} in navigation" :key="name" class="hover:text-white transition">
+                <a :href="href">{{ name }}</a>
+              </li>
             </ul>
 
             <button class="border rounded py-3 px-8">
