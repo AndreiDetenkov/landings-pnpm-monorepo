e830a20ef9178977141482fe84afedcede7ae67d
AndreiDetenkov <32195682+AndreiDetenkov@users.noreply.github.com>
2024-01-29 22:04:39 +0600

    feat: add header component

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,8 +1,25 @@
 <script setup lang="ts">
-
 </script>
 
 <template>
-  <header>header</header>
-</template>
+  <header class="bg-gradient-to-r from-[#101820] to-[#553f38] py-8">
+      <div class="container">
+        <div class="flex justify-between">
+          <div class="text-2xl font-black text-white">Beuitty.</div>
 
+          <nav>
+            <ul class="inline-flex gap-x-14 text-[#898A92] pr-14">
+              <li class="hover:text-white transition text-white"><a href="#">Home</a></li>
+              <li class="hover:text-white transition"><a href="#">Product</a></li>
+              <li class="hover:text-white transition"><a href="#">About</a></li>
+              <li class="hover:text-white transition"><a href="#">Contact</a></li>
+            </ul>
+
+            <button class="border rounded py-3 px-8">
+              <span class="text-white">Free trial</span>
+            </button>
+          </nav>
+        </div>
+      </div>
+  </header>
+</template>
