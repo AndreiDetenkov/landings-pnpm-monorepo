27d0cbe1f61df4e95a467485decc95f3f12ffd9a
AndreiDetenkov <32195682+AndreiDetenkov@users.noreply.github.com>
2024-02-21 20:33:10 +0600

    refactor: move hero section into separate component

--- skin-care-landing/src/components/TheMain.vue
+++ skin-care-landing/src/components/TheMain.vue
@@ -1,31 +1,10 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import HeroSection from '@/components/HeroSection.vue'
+</script>
 
 <template>
   <main>
-    <section id="hero" class="bg-gradient-to-r from-primary to-brown">
-      <div class="container grid grid-cols-2 gap-4 min-h-[765px] pt-8">
-        <div class="max-w-[470px] pt-20">
-          <h1 class="text-5xl leading-[70px] text-white font-extrabold mb-6">
-            Take care of you with natural Ingredients
-          </h1>
-          <p class="text-lg text-white leading-7 mb-8">
-            It is a long established fact that a reader will be distracted by the readable content
-            of a page
-          </p>
-          <button class="border border-secondary bg-secondary text-white rounded py-3 px-8">
-            Get Started
-          </button>
-        </div>
-        <div class="relative max-h-[580px]">
-          <div
-            class="bg-figure-image bg-no-repeat bg-bottom bg-contain max-h-[373px] h-full max-w-[614px] w-full absolute bottom-0"
-          />
-          <div
-            class="bg-hero-image bg-no-repeat bg-bottom bg-contain max-h-[583px] h-full max-w-[582px] w-full absolute bottom-0 xl:left-0 2xl:left-5"
-          />
-        </div>
-      </div>
-    </section>
+    <HeroSection />
 
     <section id="quality">
       <div class="container grid grid-cols-2 gap-4">
@@ -60,4 +39,3 @@
   </main>
 </template>
 
-<style scoped></style>
